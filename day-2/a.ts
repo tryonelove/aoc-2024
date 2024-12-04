1f250cf051a8948f98d716246c15f6e5f1e2a89c
Ilya <tryonelove@proton.me>
2024-12-04 11:40:00 +0100

    solve day-2:a

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,9 +1,42 @@
 import { runSolution } from '../utils.ts';
 
+const MIN_DEVIATION = 1;
+const MAX_DEVIATION = 3;
+
+function checkIfSafeReport(report: number[]) {
+  let reportDir: 'increasing' | 'decreasing';
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const diff = report[i] - report[i + 1];
+
+    const currentDir = diff < 0 ? 'increasing' : 'decreasing';
+    if (reportDir === undefined) {
+      reportDir = currentDir;
+    }
+
+    if (reportDir !== currentDir) {
+      return false;
+    }
+
+    const absDiff = Math.abs(diff);
+
+    if (absDiff < MIN_DEVIATION || absDiff > MAX_DEVIATION) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  const lines = data.map((v) => v.split(' ').map(Number));
+
+  return lines.reduce((prev, cur) => {
+    const isSafe = checkIfSafeReport(cur);
+    console.log(cur, isSafe);
+    return prev + (isSafe ? 1 : 0);
+  }, 0);
 }
 
 await runSolution(day2a);
