a8de6a131499ba2a2dff1902e50c5bd8b87d5b4f
Ilya <tryonelove@proton.me>
2024-12-04 19:55:06 +0100

    solve day-2:b

--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,46 @@
 import { runSolution } from '../utils.ts';
 
+const MIN_DEVIATION = 1;
+const MAX_DEVIATION = 3;
+
+function checkIfSafeReport(report: number[]) {
+  let reportDir: 'increasing' | 'decreasing';
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const diff = report[i] - report[i + 1];
+
+    const currentDir = diff < 0 ? 'increasing' : 'decreasing';
+    if (reportDir === undefined) {
+      reportDir = currentDir;
+    }
+
+    if (reportDir !== currentDir) {
+      return false;
+    }
+
+    const absDiff = Math.abs(diff);
+
+    if (absDiff < MIN_DEVIATION || absDiff > MAX_DEVIATION) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 /** provide your solution as the return of this function */
 export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+  const lines = data.map((v) => v.split(' ').map(Number));
+
+  return lines.filter((line) => {
+    for (let i = 0; i < line.length; i++) {
+      if (checkIfSafeReport(line.toSpliced(i, 1))) {
+        return true;
+      }
+    }
+
+    return false;
+  }).length;
 }
 
 await runSolution(day2b);
