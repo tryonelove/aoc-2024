685448769d0d1d9bfbbe7e549b9f0b7bc8163ffa
Ilya <tryonelove@proton.me>
2024-12-04 11:19:31 +0100

    solve day-1:b

--- day-1/b.ts
+++ day-1/b.ts
@@ -2,8 +2,23 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  const first: number[] = [];
+  const second: number[] = [];
+  const firstValueCountMap = new Map<number, number>();
+  const secondValueCountMap = new Map<number, number>();
+
+  data.forEach((v) => {
+    const [v1, v2] = v.split('  ').map(Number);
+    first.push(v1);
+    second.push(v2);
+
+    firstValueCountMap.set(v1, (firstValueCountMap.get(v1) ?? 0) + 1);
+    secondValueCountMap.set(v2, (secondValueCountMap.get(v2) ?? 0) + 1);
+  });
+
+  return first.reduce((prev, cur) => {
+    return prev + cur * (secondValueCountMap.get(cur) ?? 0);
+  }, 0);
 }
 
 await runSolution(day1b);
