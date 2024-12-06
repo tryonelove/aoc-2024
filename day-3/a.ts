6f3e216245fbe9a25d32ee052c1485c1edb03c96
Ilya <tryonelove@proton.me>
2024-12-07 00:51:44 +0100

    solve day 3 b :))))))))))))))))))))))))))))))

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,22 @@
 import { runSolution } from '../utils.ts';
 
+const MUL_PATTERN = /mul\((\w+),(\w+)\)/g;
+
+function parseMul(line: string) {
+  const parts = [...line.matchAll(MUL_PATTERN)];
+
+  return parts.reduce((prev, cur) => {
+    const [, a, b] = cur;
+
+    return prev + Number(a) * Number(b);
+  }, 0);
+}
+
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  return data.reduce((prev, cur) => {
+    return prev + parseMul(cur);
+  }, 0);
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -1,9 +1,45 @@
 import { runSolution } from '../utils.ts';
 
+const IMPORTANT_PARTS = /mul\(\d+,\d+\)|do\(\)|don't\(\)/g;
+const MUL_PATTERN = /mul\((\d+),(\d+)\)/;
+
+const flags = {
+  do: 'do()',
+  dont: "don't()",
+} as const;
+
+function parseMul(line: string) {
+  const match = line.match(MUL_PATTERN);
+  if (match) {
+    const [, a, b] = match;
+    return Number(a) * Number(b);
+  }
+  return 0;
+}
+
+function getMulSum(line: string) {
+  const matchResults = [...line.matchAll(IMPORTANT_PARTS)];
+  let enabled = true;
+  let sum = 0;
+
+  for (const match of matchResults) {
+    const instruction = match[0];
+
+    if (instruction === flags.do) {
+      enabled = true;
+    } else if (instruction === flags.dont) {
+      enabled = false;
+    } else if (enabled) {
+      sum += parseMul(instruction);
+    }
+  }
+
+  return sum;
+}
+
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+  return getMulSum(data.join(''));
 }
 
 await runSolution(day3b);
