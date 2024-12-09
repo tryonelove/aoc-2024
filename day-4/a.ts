8a4673ba24bd3d090937631248d3f2a357f63e02
Ilya <tryonelove@proton.me>
2024-12-09 12:18:23 +0100

    solve day-4

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,96 @@
 import { runSolution } from '../utils.ts';
 
+const xmas = 'XMAS';
+// By default, let's consider the grid direction is left to right - 0 degrees
+
+type Position = {
+  row: number;
+  col: number;
+};
+
+function isXmas(word: string) {
+  return word === xmas;
+}
+
+function toReversed(word: string) {
+  return word.split('').toReversed().join('');
+}
+
+function getHorizontal(data: string[], position: Position) {
+  return data[position.row].substring(position.col, position.col + xmas.length);
+}
+
+function getDiagonalRight(data: string[], position: Position) {
+  return Array.from({ length: xmas.length }).reduce<string>((prev, _, i) => {
+    const row = data[position.row + i];
+    if (!row) {
+      return prev;
+    }
+
+    const col = row[position.col + i];
+    if (!col) {
+      return prev;
+    }
+
+    return prev + col;
+  }, '');
+}
+
+function getDiagonalLeft(data: string[], position: Position) {
+  return Array.from({ length: xmas.length }).reduce<string>((prev, _, i) => {
+    const row = data[position.row + i];
+    if (!row) {
+      return prev;
+    }
+
+    const col = row[position.col - i];
+    if (!col) {
+      return prev;
+    }
+
+    return prev + col;
+  }, '');
+}
+
+function getDownwards(data: string[], position: Position) {
+  return Array.from({ length: xmas.length }).reduce<string>((prev, _, i) => {
+    const row = data[position.row + i];
+    if (!row) {
+      return prev;
+    }
+
+    const col = row[position.col];
+    if (!col) {
+      return prev;
+    }
+
+    return prev + col;
+  }, '');
+}
+
+function getXmaxCountInWindow(data: string[], position: Position) {
+  const words = [
+    getHorizontal(data, position),
+    getDiagonalRight(data, position),
+    getDiagonalLeft(data, position),
+    getDownwards(data, position),
+  ];
+
+  const reversedWords = words.map(toReversed);
+
+  return words.filter(isXmas).length + reversedWords.filter(isXmas).length;
+}
+
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      count += getXmaxCountInWindow(data, { col: j, row: i });
+    }
+  }
+
+  return count;
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,77 @@
 import { runSolution } from '../utils.ts';
 
+const xmas = 'MAS';
+
+type Position = {
+  row: number;
+  col: number;
+};
+
+function isXmas(word: string) {
+  return word === xmas;
+}
+
+function toReversed(word: string) {
+  return word.split('').toReversed().join('');
+}
+
+function getDiagonalRight(data: string[], position: Position) {
+  return Array.from({ length: xmas.length }).reduce<string>((prev, _, i) => {
+    const row = data[position.row + i];
+    if (!row) {
+      return prev;
+    }
+
+    const col = row[position.col + i];
+    if (!col) {
+      return prev;
+    }
+
+    return prev + col;
+  }, '');
+}
+
+function getDiagonalLeft(data: string[], position: Position) {
+  return Array.from({ length: xmas.length }).reduce<string>((prev, _, i) => {
+    const row = data[position.row + i];
+    if (!row) {
+      return prev;
+    }
+
+    const col = row[position.col - i];
+    if (!col) {
+      return prev;
+    }
+
+    return prev + col;
+  }, '');
+}
+
+function getXmaxCountInWindow(data: string[], position: Position) {
+  const diagonalRight = getDiagonalRight(data, position);
+  const diagonalLeft = getDiagonalLeft(data, {
+    col: position.col + 2,
+    row: position.row,
+  });
+
+  const isDiagonalRightXmas =
+    isXmas(diagonalRight) || isXmas(toReversed(diagonalRight));
+  const isDiagonalLeftXmas =
+    isXmas(diagonalLeft) || isXmas(toReversed(diagonalLeft));
+
+  return isDiagonalLeftXmas && isDiagonalRightXmas ? 1 : 0;
+}
+
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      count += getXmaxCountInWindow(data, { col: j, row: i });
+    }
+  }
+
+  return count;
 }
 
 await runSolution(day4b);
